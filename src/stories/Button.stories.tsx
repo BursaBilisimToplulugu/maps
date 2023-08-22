510e9d9251f783e6ab4d6ad0153bff080240a48f
Utku Demir <50655392+Demir-Utku@users.noreply.github.com>
2023-08-22 18:23:13 +0300

    feat(button): create story file

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,115 @@
+import Button from '@/app/common/components/Button';
+import Icon from '@/app/common/components/Icon';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['neutral', 'light', 'dark'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['small', 'medium'],
+      control: { type: 'radio' },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// Variants
+export const Neutral: Story = {
+  args: {
+    variant: 'neutral',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Neutral</Button>,
+};
+
+export const Light: Story = {
+  args: {
+    variant: 'light',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Light</Button>,
+};
+
+export const Dark: Story = {
+  args: {
+    variant: 'dark',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Dark</Button>,
+};
+
+// Sizes
+export const Small: Story = {
+  args: {
+    variant: 'neutral',
+    size: 'small',
+    disabled: false,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Small</Button>,
+};
+
+export const Medium: Story = {
+  args: {
+    variant: 'neutral',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Medium</Button>,
+};
+
+// Disabled
+export const Disabled: Story = {
+  args: {
+    variant: 'neutral',
+    size: 'medium',
+    disabled: true,
+  },
+  render: ({ ...rest }) => <Button {...rest}>Disabled</Button>,
+};
+
+// Left Icon
+export const LeftIcon: Story = {
+  args: {
+    variant: 'neutral',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => (
+    <Button {...rest}>
+      <Icon.Star />
+      Button
+    </Button>
+  ),
+};
+
+// Right Icon
+export const RightIcon: Story = {
+  args: {
+    variant: 'light',
+    size: 'medium',
+    disabled: false,
+  },
+  render: ({ ...rest }) => (
+    <Button {...rest}>
+      Button
+      <Icon.Star />
+    </Button>
+  ),
+};
