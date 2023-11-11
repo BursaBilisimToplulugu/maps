8dc5a50e154a484788553113f0bc1060bc4b79da
borabiricik <bora.bir496@gmail.com>
2023-11-12 02:13:04 +0300

    feat: switch component added

--- src/app/common/components/Switch/index.tsx
+++ src/app/common/components/Switch/index.tsx
@@ -2,36 +2,34 @@ import classNames from 'classnames';
 import { motion } from 'framer-motion';
 
 interface Props {
-  size?: 'small' | 'medium' | 'large';
-  isActive: boolean;
+  isActive?: boolean;
   onClick?: (isActive: boolean) => void;
 }
 
-const Switch = ({ size, isActive, onClick }: Props) => {
+const Switch = ({ isActive = false, onClick }: Props) => {
   return (
     <motion.button
       type="button"
       className={classNames(
-        'w-[64px] h-[32px] rounded-full  relative',
-        isActive ? 'bg-primary-green' : 'bg-neutrals-white'
+        'w-[64px] h-[32px] rounded-full transition-colors duration-300 border ',
+        isActive
+          ? 'bg-primary-green border-primary-green'
+          : 'bg-neutrals-white border-neutrals-navyWhite'
       )}
-      animate={{
-        backgroundColor: isActive ? '#58C27D' : '#F4F5F6',
-      }}
       onClick={() => {
         onClick?.(!isActive);
       }}
     >
       <motion.div
-        className=" absolute top-1/2 w-6 h-6 flex items-center justify-center"
+        className={classNames(
+          'rounded-full w-5 h-5 mx-2 transition-colors duration-300',
+          isActive ? 'bg-white' : 'bg-neutrals-navyGrey'
+        )}
         animate={{
-          left: isActive ? '100%' : '0%',
+          marginLeft: isActive ? '90%' : '10%',
           translateX: isActive ? '-100%' : '0%',
-          translateY: '-50%',
         }}
-      >
-        <div className={classNames('bg-white rounded-full w-5 h-5 px-2')}></div>
-      </motion.div>
+      />
     </motion.button>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontSize: {
@@ -54,32 +55,31 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         primary: {
-          navyBlue: "#3B71FE",
-          blue: "#8BC5E5",
-          purple: "#92A5EF",
-          green: "#58C27D",
+          navyBlue: '#3B71FE',
+          blue: '#8BC5E5',
+          purple: '#92A5EF',
+          green: '#58C27D',
         },
         secondary: {
-          lightBlue: "#A4CDE3",
-          nude: "#E4D7CF",
-          yellow: "#FFD166",
-          orange: "#FA8F54",
+          lightBlue: '#A4CDE3',
+          nude: '#E4D7CF',
+          yellow: '#FFD166',
+          orange: '#FA8F54',
         },
         neutrals: {
-          dark: "#141416",
-          navyDark: "#141416",
-          darkGrey: "#353945",
-          navyGrey: "#777E90",
-          grey: "#B1B5C3",
-          darkWhite: "#E6E8EC",
-          white: "#F4F5F6",
-          navyWhite: "#E6E8EC",
+          dark: '#141416',
+          navyDark: '#141416',
+          darkGrey: '#353945',
+          navyGrey: '#777E90',
+          grey: '#B1B5C3',
+          white: '#F4F5F6',
+          navyWhite: '#E6E8EC',
         },
       },
     },
