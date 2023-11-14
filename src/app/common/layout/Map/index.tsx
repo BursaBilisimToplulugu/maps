733fe1031f6cb5b5c4d19ab09138da8db0976d19
borabiricik <bora.bir496@gmail.com>
2023-11-14 23:55:57 +0300

    feat: smooth zoom added

--- src/app/common/layout/Map/index.tsx
+++ src/app/common/layout/Map/index.tsx
@@ -1,16 +1,18 @@
 'use client';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import { memo, useEffect, useState } from 'react';
+import { smoothZoom } from './utils/smoothZoom';
 
 type Props = {};
 
 //deployment stage
 
 const Map = (props: Props) => {
+  const [map, setMap] = useState<google.maps.Map | null>(null);
   const [location, setLocation] = useState<GeolocationCoordinates | null>(null);
   const [center, setCenter] = useState<{ lat: number; lng: number }>({
-    lat: 40.222568,
-    lng: 28.820867,
+    lat: 40.1885,
+    lng: 29.061,
   });
   const [zoom, setzoom] = useState(12);
   const { isLoaded } = useJsApiLoader({
@@ -47,16 +49,25 @@ const Map = (props: Props) => {
       lat: location?.latitude || 40.222568,
       lng: location?.longitude || 28.820867,
     });
-    setzoom(16);
+    smoothZoom(zoom, 16, setzoom);
   };
   return isLoaded ? (
     <GoogleMap
+      onLoad={(map) => {
+        setMap(map);
+      }}
       mapContainerClassName="w-full min-safe-h-screen"
       center={center}
       zoom={zoom}
+      onZoomChanged={() => {
+        const zoom = map?.getZoom();
+        if (map && zoom) {
+          setzoom(zoom);
+        }
+      }}
       options={{
         mapTypeControl: false,
-        fullscreenControl: true,
+        fullscreenControl: false,
         zoomControl: false,
         streetViewControl: false,
         gestureHandling: 'greedy',
@@ -79,9 +90,14 @@ const Map = (props: Props) => {
           className: '-mt-8 !text-primary-navyBlue font-bold',
         }}
       />
-      <button onClick={locateMe} className="absolute bottom-5 right-5 bg-white">
-        click me
-      </button>
+      {location && location?.latitude > 0 && location?.longitude > 0 && (
+        <button
+          onClick={locateMe}
+          className="absolute bottom-5 right-5 bg-white"
+        >
+          click me
+        </button>
+      )}
     </GoogleMap>
   ) : (
     <></>
