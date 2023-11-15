d8fe879ed9203cb7063ec3ebe78ef14fa2bf041e
mertcetiin <mrtctn506@gmail.com>
2023-11-15 17:45:13 +0300

    feat: added checkMark icon

--- src/app/common/components/Icon/index.tsx
+++ src/app/common/components/Icon/index.tsx
@@ -1,3 +1,4 @@
+import CheckMark from '@/app/common/assets/icons/CheckMark';
 import OneTwoThree from '@/app/common/assets/icons/OneTwoThree';
 import Star from '@/app/common/assets/icons/Star';
 import { SVGProps } from 'react';
@@ -7,6 +8,7 @@ export interface IconProps extends SVGProps<SVGSVGElement> {}
 const Icon = {
   OneTwoThree: OneTwoThree,
   Star,
+  CheckMark,
 };
 
 export default Icon;
