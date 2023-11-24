0b86c5bfd645a667dbd7dc61af4f095776ff61ec
mertcetiin <mrtctn506@gmail.com>
2023-11-24 22:13:33 +0300

    fix: conflick

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,11 +1,9 @@
-import Checkbox from '../common/components/Checkbox';
 import Icon from '../common/components/Icon';
 
 export default function Home() {
   return (
     <div>
       Welcome home !asd <Icon.OneTwoThree />
-      <Checkbox label="value" />
     </div>
   );
 }
