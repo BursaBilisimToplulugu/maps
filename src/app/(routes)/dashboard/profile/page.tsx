a775fa2ced0c5a132fc8b4aea06930eaf5ec4f5b
borabiricik <bora.bir496@gmail.com>
2023-12-27 21:37:17 +0300

    fix: profile flow added again

--- src/app/(routes)/dashboard/profile/page.tsx
+++ src/app/(routes)/dashboard/profile/page.tsx
@@ -1,12 +1,16 @@
+import { getSession } from '@/app/common/utils/getSession';
+import ProfileForm from './components/ProfileForm';
+
 type Props = {};
 
 export const dynamic = 'force-dynamic';
 
 const ProfilePage = async (props: Props) => {
+  const { user } = await getSession();
   return (
     <div className="">
       <h2 className="text-3xl mb-10">Profil</h2>
-      {/* <ProfileForm user={data} /> */}
+      <ProfileForm user={user} />
     </div>
   );
 };
